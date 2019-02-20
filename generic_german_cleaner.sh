05f370ff23c3aecfdf47bc4027683540409e9efd
Vijay Ravichandran <rvijay1234@gmail.com>
2019-02-20 14:50:08 +0100

    Add generic_german_cleaner.sh

---
+++ generic_german_cleaner.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Desc: Generic cleaner for text corpora. 
+#       Keeps all punctuations and all acceptable german letters and special characters
+
+# Usage: ./generic_german_cleaner.sh -i <input_file> -o <output_file>
+
+# Regex rules in place
+# 1. Remove all characters except [^A-Za-z0-9äöüÄÖÜßé\’\„\“\«\»\“\”\'\´\"\.\?\!\:\;\,\ \t\n\@\%\&\!\$\€\£\§\°\#\|\>\<\\\/\^\~\(\)\{\}\+\*\=(&amp)\_\–\—\-\—\–\-]
+# 2. Remove \- \— \– when not sandwiched between words such as Text-to-Speech or when before a number such as -10. 
+#    This is done using regex "-(?=[\s\-—–])|(?<=[\s\-—–])-(?=[^0-9])|—(?=[\s\-—–])|(?<=[\s\-—–])—(?=[^0-9])|–(?=[\s\-—–])|(?<=[\s\-—–])–(?=[^0-9])". 
+#    Sadly, lookahead andlookbehind are not supported by sed, so we have to use perl.
+# 3. Replace &amp by & -------------> Found a lot in HTML pages
+# 4. Remove all empty lines
+
+
+while getopts ":i:o:" option; do
+    case "${option}" in
+        i)
+            INPUT_FILE=${OPTARG}
+            ;;
+        o)
+            OUTPUT_FILE=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+echo "Input file: ${INPUT_FILE}"
+echo "Output file: ${OUTPUT_FILE}"
+
+
+cat $INPUT_ | sed -r "s/[^]A-Za-z0-9äöüÄÖÜßé\’\„\“\«\»\“\”\'\´\"\.\?\!\:\;\,\ \t\n\@\%\&\!\$\€\£\§\°\#\|\>\<\\\/\^\~\(\)\{\}\+\*\=(&amp)\_\–\—\-\—\–\-\[]//g" | sed 's/\&amp/\&/g'  | perl -C -pe "s/[-—–](?=[\s\-—–])|(?<=[\s\-—–])[-—–](?=[^0-9])//g" | sed '/^\s*$/d' >  $output_filename  &
